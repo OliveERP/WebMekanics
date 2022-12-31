32eee3c867c23628610d0ddfb528e2b5ed7e7ac7
OliveERP <120718232+OliveERP@users.noreply.github.com>
2023-01-01 02:35:07 +0500

    Getting Upload link of Image from Onedrive

---
+++ Scripts For ERPNext/Client Script/item.js
@@ -0,0 +1,26 @@
+frappe.ui.form.on('Item', {
+	refresh(frm) {
+      cur_frm.add_custom_button(__('Upload Image'), () => {
+          frappe.prompt({
+                label: 'Drive URL',
+                fieldname: 'drive_url',
+                fieldtype: 'Text'
+            }, (value) => {
+                frappe.call({
+                    method: "volods.volods.functions.get_image_url",
+                    args:{
+                        drive_url: value.drive_url
+            		},
+            		callback:function(r){
+            		    if(r.message ){
+            		        console.log(r.message);
+            		        cur_frm.set_value('image', r.message);
+            		        frm.save();
+            		    }
+            		}
+            	});
+                
+            });
+	    }).addClass('btn-primary');
+   } 
+})
