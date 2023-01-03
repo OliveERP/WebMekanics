462dfedf8dcd1a744177ddd87626de12692119c9
OliveERP <120718232+OliveERP@users.noreply.github.com>
2023-01-03 20:14:08 +0500

    Making Necessary changes in Controller

--- Scripts For ERPNext/Client Script/purchase_invoice.js
+++ Scripts For ERPNext/Client Script/purchase_invoice.js
@@ -4,7 +4,7 @@
 // Enabled: Click this checkbox
 // Script: Paste the following code in Script section and SAVE the document
 
-erpnext.buying.ItemListCustom = erpnext.buying.PurchaseInvoiceController.extend({
+erpnext.buying.ItemListCustom = erpnext.accounts.PurchaseInvoice.extend({
     setup_queries:function(doc, dt, dn){
        this._super();
        var me  = this
@@ -37,14 +37,14 @@ frappe.ui.form.on('Purchase Invoice Item', {
 	refresh(frm) {
 		// your code here
 	},
-	iitem_code(frm,cdt,cdn) {
+	item_code(frm,cdt,cdn) {
 	    var row = locals[cdt][cdn];
     	frappe.model.with_doc("Item", row.item_code, function() {
 	        var file = frappe.model.get_doc("Item", row.item_code);
             row.image_view_html = repl('<img src="%(imgurl)s" width="31px">', {imgurl: file.image});
-            if (!file){
-                 row.image_view_html = repl('<img src="%(imgurl)s" width="31px">', {imgurl: row.image});
-            }
+            // if (!file){
+            //      row.image_view_html = repl('<img src="%(imgurl)s" width="31px">', {imgurl: row.image});
+            // }
             //frm.refresh_field('items');
     	        
     	   });
