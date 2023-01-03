c5b91bbeaa6d7f5bd85bcc077cb964c4b999b626
OliveERP <120718232+OliveERP@users.noreply.github.com>
2023-01-04 00:31:52 +0500

    Code Update

--- Scripts For ERPNext/Client Script/pick_list.js
+++ Scripts For ERPNext/Client Script/pick_list.js
@@ -34,9 +34,9 @@ frappe.ui.form.on('Pick List Item', {
     	frappe.model.with_doc("Item", row.item_code, function() {
 	        var file = frappe.model.get_doc("Item", row.item_code);
             row.image_view_html = repl('<img src="%(imgurl)s" width="31px">', {imgurl: file.image});
-            if (!file){
-                 row.image_view_html = repl('<img src="%(imgurl)s" width="31px">', {imgurl: row.image});
-            }
+            //if (!file){
+            //     row.image_view_html = repl('<img src="%(imgurl)s" width="31px">', {imgurl: row.image});
+            //}
             //frm.refresh_field('items');
     	        
     	   });
