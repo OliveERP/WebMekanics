be3bbd3b4bd36f48b2479622ad302d1af5229af1
OliveERP <120718232+OliveERP@users.noreply.github.com>
2023-01-03 18:58:32 +0500

    Setting its original functionality

--- Scripts For ERPNext/Client Script/supplier_quotation.js
+++ Scripts For ERPNext/Client Script/supplier_quotation.js
@@ -4,25 +4,32 @@
 // Enabled: Click this checkbox
 // Script: Paste the following code in Script section and SAVE the document
 
-cur_frm.cscript.refresh = function(frm) {
-       
-        cur_frm.set_query("item_code", "items", function () {
-            return {
-                query: "volods.volods.item_query.item_search",
-                filters: { 'is_sales_item': 1 }
-            };
-        });
-    }
-   cur_frm.cscript.onload =  function(frm) {
-        
-        cur_frm.set_query("item_code", "items", function () {
-            return {
-                query: "volods.volods.item_query.item_search",
-                filters: { 'is_sales_item': 1 }
-            };
-        });
-    }
+erpnext.buying.ItemListCustom = erpnext.buying.SupplierQuotationController.extend({
+    setup_queries:function(doc, dt, dn){
+       this._super();
+       var me  = this
+      if(this.frm.fields_dict["items"].grid.get_field('item_code')) {
+          this.frm.set_query("item_code", "items", function() {
+			if (me.frm.doc.is_subcontracted == "Yes") {
+				return{
+					query: "volods.volods.item_query.item_search",
+					filters:{ 'is_sub_contracted_item': 1 }
+				}
+			}
+			else {
+				return{
+					query: "volods.volods.item_query.item_search",
+					filters: {'is_purchase_item': 1}
+				}
+			}
+          })
+			
+		}
+    },
+   
+})
 
+$.extend(cur_frm.cscript, new erpnext.buying.ItemListCustom({frm: cur_frm}));
 
 
 frappe.ui.form.on('Supplier Quotation Item', {
